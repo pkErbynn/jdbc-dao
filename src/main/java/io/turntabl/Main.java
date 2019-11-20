0e6a5c16171d52b4177c0ac61cc2b2aaf9f9fe07
pkErbynn <john.erbynn@gmail.com>
2019-11-20 02:06:12 +0000

    Main impl

--- src/main/java/io/turntabl/Main.java
+++ src/main/java/io/turntabl/Main.java
@@ -10,58 +10,32 @@ import java.util.Date;
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) {
 //        JDBC jdbc = new JDBC();
 //        jdbc.readCustomerData();
 
         // testing if config == success
 //        ApplicationContext ctx = new FileSystemXmlApplicationContext("beans.xml");
         ApplicationContext appctx = new ClassPathXmlApplicationContext("beans.xml");
-        JdbcTemplate template = (JdbcTemplate) appctx.getBean("jdbcTemplate");
 
+        JdbcTemplate template = (JdbcTemplate) appctx.getBean("jdbcTemplate");
         int nr = template.queryForObject("select count(contact_name) from customers", Integer.class);
         System.out.println("count: " + nr);
 
+        CustomerDAOImpl customerTemplate = (CustomerDAOImpl) appctx.getBean("customerDAOTemplate");
+        System.out.println("All customers: " + customerTemplate.getAllCustomers());
+        System.out.println("Customers count: " + customerTemplate.numberOfCustomers());
+        System.out.println("Ony Customers: " +customerTemplate.getCustomerByName("John Leet"));
 
 
+        ProductDAOImpl productDAOTemp = (ProductDAOImpl) appctx.getBean("productDAOTemplate");
+        System.out.println("All products: " + productDAOTemp.getAllProducts());
+        System.out.println("All products: " + productDAOTemp.showAllProductsByCustomer("Karin Josephs"));
+        System.out.println("Top 5: " + productDAOTemp.displayTop5PopularProducts());
 
 
-
-
-
-
-        /*System.out.println("Enter name: >>> ");
-        Scanner inp = new Scanner(System.in);
-        String name = inp.nextLine();
-        jdbc.searchCustomerName(name);*/
-//        jdbc.searchCategoryByName("Beverages");
-//        jdbc.searchCategoryByName(name);
-
-
-//        dao
-//        ProductDAOImpl productDAO = new ProductDAOImpl();
-//        productDAO.showAllProductsByCustomer("Karin Josephs");
-//        for (ProductTO prod : productDAO.showAllProductsByCustomer("Karin Josephs")) {
-//            System.out.printf("%10s %50s", prod.getProductName(), prod.getPrice());
-//            System.out.println();
-//        }
-
-        // products bought by customer
-  /*      productDAO.showAllProductsByCustomer("Karin Josephs").stream()
-                .forEach(c -> System.out.printf( "%30s %20s", c.getProductName(), c.getPrice() + "\n"));
-*/
-//        sales report
-/*
-        SalesDAOImpl salesDAO = new SalesDAOImpl();
-        salesDAO.weeklySalesReport(new Date(1996 ,6 ,10)).stream()
-                .forEach(e -> System.out.printf("%10s %35s %25s %20s"
-                        , e.getOrderDate()
-                        , e.getCustomerName()
-                        , e.getProductName()
-                        , e.getAmount())
-                );
-
-*/
+        SalesDAOImpl sales = (SalesDAOImpl) appctx.getBean("salesDAOTemp");
+        System.out.println("Sales: " + sales.getSales());
 
 
     }
