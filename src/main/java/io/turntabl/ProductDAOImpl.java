3bdf08932647cc17237023b27136ce4f676bfffb
pkErbynn <john.erbynn@gmail.com>
2019-11-20 07:10:29 +0000

    query rename...

--- src/main/java/io/turntabl/ProductDAOImpl.java
+++ src/main/java/io/turntabl/ProductDAOImpl.java
@@ -40,7 +40,7 @@ public class ProductDAOImpl implements ProductDAO {
         JdbcTemplate jTemplate = (JdbcTemplate) appContx.getBean("productDAOTemplate");
 
         RowMapper rowMapper = BeanPropertyRowMapper.newInstance(ProductTO.class);
-        String CUSTOMER_PROD_QUERY = "select product_name, order_details.unit_price from products \" +\n" +
+        String SELECT_CUSTOMER_PROD_QUERY = "select product_name, order_details.unit_price from products \" +\n" +
                 "                        \"inner join order_details on products.product_id = order_details.product_id \" +\n" +
                 "                        \"inner join orders on order_details.order_id = orders.order_id \" +\n" +
                 "                        \"inner join customers on orders.customer_id = customers.customer_id where customers.contact_name = ? limit 5";
@@ -51,7 +51,7 @@ public class ProductDAOImpl implements ProductDAO {
         */
 
         List<ProductTO> custProds = jTemplate.query(
-                CUSTOMER_PROD_QUERY,
+                SELECT_CUSTOMER_PROD_QUERY,
 //                new Object[]{"Thomas Hardy"},
                 new Object[]{customerName},
                 rowMapper
@@ -66,13 +66,13 @@ public class ProductDAOImpl implements ProductDAO {
         JdbcTemplate jTemplate = (JdbcTemplate) appContx.getBean("productDAOTemplate");
 
         RowMapper rowMapper = BeanPropertyRowMapper.newInstance(ProductTO.class);
-        final String top5ProdQuery = "select products.product_name, products.unit_price, count(products.product_name) as pc " +
+        final String SELECT_PROD_QUERY = "select products.product_name, products.unit_price, count(products.product_name) as pc " +
                 "from products inner join order_details " +
                 "on products.product_id = order_details.product_id " +
                 "group by products.product_name, products.unit_price " +
                 "order by pc " +
                 "desc limit 5";
-        List<ProductTO> popularProds = jTemplate.query(top5ProdQuery, rowMapper);
+        List<ProductTO> popularProds = jTemplate.query(SELECT_PROD_QUERY, rowMapper);
         return popularProds;
     }
 
